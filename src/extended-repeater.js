10af1eeae1f6ac378fc732f7767f31b346766a0e
Probluesea <danil.kriw@gmail.com>
2023-12-18 19:43:47 +0500

    feat: implement extended-repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,38 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options = {}) {
+  str = String(str);
+
+  const opt = {
+    repeatTimes: options.repeatTimes ? options.repeatTimes : 1,
+    separator:
+      options.separator?.toString() || options.separator === null ? String(options.separator) : "+",
+    addition:
+      options.addition?.toString() || options.addition === null ? String(options.addition) : "",
+    additionRepeatTimes: options.additionRepeatTimes ? options.additionRepeatTimes : 1,
+    additionSeparator:
+      options.additionSeparator?.toString() || options.additionSeparator === null
+        ? String(options.additionSeparator)
+        : "|",
+  };
+
+  console.log(str, opt);
+
+  let result = [];
+  for (let i = 0; i < opt.repeatTimes; i++) {
+    let item = [`${str}`];
+    let subArr = [];
+    for (let j = 0; j < opt.additionRepeatTimes; j++) {
+      subArr.push(`${opt.addition}`);
+    }
+    const addition = subArr.join(opt.additionSeparator);
+    item.push(addition);
+
+    result.push(item.join(""));
+  }
+
+  return result.join(opt.separator);
 }
 
 module.exports = {
